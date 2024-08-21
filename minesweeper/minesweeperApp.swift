e455c620f3977516a584172ee89ebc1171e59364
Daniel Ben Avi <daniel.ben.avi10@gmail.com>
2024-08-21 17:55:00 +0300

    Initial Commit

---
+++ minesweeper/minesweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  minesweeperApp.swift
+//  minesweeper
+//
+//  Created by דניאל בן אבי on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct minesweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
